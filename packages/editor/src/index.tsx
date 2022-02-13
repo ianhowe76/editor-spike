c6217c25267089edf802f102d2c4abd88f459799
Ian Howe <ian@vbizapp.com>
2022-02-14 07:37:08 +1100

    feat(editor): Add editor package

---
+++ packages/editor/src/index.tsx
@@ -0,0 +1,19 @@
+import React, { useState, ChangeEvent } from "react";
+
+interface IEditorProps {
+  initialText: string;
+  onSave: (text: string) => void;
+}
+
+export const Editor: React.FC<IEditorProps> = ({ initialText, onSave }) => {
+  const [text, setText] = useState<string>(initialText);
+
+  return (
+    <div>
+      <input type="textbox" value={text} onChange={(e: ChangeEvent<HTMLInputElement>) => setText(e.target.value)} />
+      <button onChange={() => onSave(text)} />
+    </div>
+  )
+};
+
+export default Editor;
